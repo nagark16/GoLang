dd02d6a45b2f235e09b1a99be290311210fed9dd
nagark16 <cnrkrishna@gmail.com>
2018-08-31 16:36:23 -0400

    learn new basics

--- basics.go
+++ basics.go
@@ -4,6 +4,41 @@ import ("fmt"
 		"math"
 		"math/rand")
 
+/*
+	2 types of methods	
+		1. value receivers
+		2. pointer receivers
+*/
+
+const usixteenbitmax float64 = 65535
+const kmh_multiple float64 = 1.60934
+
+type car struct{
+	gas_pedal uint16 // min 0 max 65535
+	brake_pedal uint16
+	steering_wheel int16 //-32k to +32k
+	top_speed_kmh float64
+}
+
+//value receiver methods for car struct
+func (c car) kmh() float64{
+	return float64(c.gas_pedal) * (c.top_speed_kmh/usixteenbitmax)
+}
+
+func (c car) mph() float64{
+	return float64(c.gas_pedal) * (c.top_speed_kmh/usixteenbitmax/kmh_multiple)
+}
+
+//pointer receiver
+func (c *car) new_top_speed (newspeed float64){
+	c.top_speed_kmh = newspeed
+}
+
+func newer_top_speed(c car, speed float64) car{
+	c.top_speed_kmh = speed
+	return c
+}
+
 //func add(x float64, y float64) float64{
 func add(x , y float64) float64{
 	return x + y
@@ -51,4 +86,24 @@ func main() {
 	fmt.Println(y)
 	*c = *c**c
 	fmt.Println(y)
+
+
+	a_car := car{gas_pedal: 22341, 
+				brake_pedal: 0, 
+				steering_wheel: 12561, 
+				top_speed_kmh: 225.0}
+
+	b_car := car{22341, 0, 12561, 225.0}
+
+	fmt.Println(a_car.gas_pedal)
+	fmt.Println(b_car.top_speed_kmh)
+
+	fmt.Println(a_car.kmh())
+	fmt.Println(a_car.mph())
+
+	a_car.new_top_speed(500)
+	fmt.Println(a_car.kmh())
+	fmt.Println(a_car.mph())
+
+	a_car = newer_top_speed(a_car, 500)
 }
