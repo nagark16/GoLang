07821448fa59a97551bf96ef53251e2533c64e06
nagark16 <cnrkrishna@gmail.com>
2018-08-31 20:19:08 -0400

    learn few more things about web

--- web.go
+++ web.go
@@ -4,7 +4,14 @@ import ("fmt"
 		"net/http")
 
 func index_handler(writer http.ResponseWriter, request *http.Request){ //* mean reading through http request
-	fmt.Fprintf(writer, "Go is neat")
+	//fmt.Fprintf(writer, "Go is neat")
+	fmt.Fprintf(writer, "<h1>Go is neat</h1>")
+	fmt.Fprintf(writer, "<p>Go is neat</p>")
+	fmt.Fprintf(writer, "<p>%s is %s</p>", "Go", "<strong>neat</strong>")
+
+	fmt.Fprintf(writer,`<h1>Go is neat</h1>
+				<p>Go is neat</p>
+		`)
 }
 
 func about_handler(writer http.ResponseWriter, request *http.Request){
